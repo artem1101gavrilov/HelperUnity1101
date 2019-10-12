0b373560afd98c60cff2fe05088e7ae372c276c8
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-12 23:25:13 +0300

    Add files via upload

---
+++ HelperUnity1101.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace HelperUnity1101
+{
+    public static class GameObject1101
+    {
+        public static void SwitchActivity(this GameObject gameObject)
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
